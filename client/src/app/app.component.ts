98770384609529c03991acc1bfb4029f3491032d
Emma <emmarraguila@comunidad.unam.mx>
2022-07-22 01:43:34 -0500

    fix: merge conflicts

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ZipCodeService } from './services/zip-code.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Elegibilidad-proyectos-de-carbono';
+  
+  ngOnInit(): void{
+    const service = new ZipCodeService()
+    console.log(service.getZipCodeInfo(54020))
+  }
 }
